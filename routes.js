3db9e26efde1c63ea009747aac5abe8fb7319356
todddrinkwater <daniel-reason>
2017-05-05 08:40:38 +1200

    add variables to route

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const people = require('./people.json')
 const fs = require('fs')
 
+
 module.exports = router
 
 // ----- Routes ----- //
@@ -16,14 +17,25 @@ router.get('/mural', (req, res) => {
   res.send('im a mural')
 })
 
-router.get('/submission', (req, res) => {
-  res.send('im a form')
-})
+// router.get('/submission', (req, res) => {
+//   res.send('im a form')
+// })
 
 router.get('/person/:id', (req, res) => { // this may change to query strings
   res.send('im a person')
+  var person = findPeople(req.params.id)
+  res.render('person/edit', person)
 })
 
-router.get('/person/:id/edit', (req, res) => { // this may change to query strings
+router.post('/person/:id/edit', (req, res) => { // this may change to query strings
   res.send('im a person editor')
+  console.log(req.body);
+  var person = findPeople(req.params.id)
+  for(key in req.body )
+  console.log(person[key]);
+}
 })
+
+findPeople = function(id) {
+  return people.find((person) => person.id == id)
+}
