9428bf68bcddba3f488ea7edbc6e532810f7c68e
Thomasin Abraham <thomasinabraham@me.com>
2017-05-05 12:05:12 +1200

    Add more tests for rendering

--- tests/mural.test.js
+++ tests/mural.test.js
@@ -16,7 +16,17 @@ test('mural page', (t) => {
   request(app)
     .get('/mural')
     .end((err, res) => {
+      $ = cheerio.load(res.text)
+      // ----- //
       t.equal(res.status, 200, "Mural page loading")
+      // ----- //
+      let actual = $(".person").length
+      t.equal(actual, 21, "Everyone displaying")
+      // ----- //
+      actual = $(".name").first().text()
+      let expected = "JV"
+      t.equal(actual, expected, "First name is what it's supposed to be")
+      // ----- //
       t.end()
     })
 })
